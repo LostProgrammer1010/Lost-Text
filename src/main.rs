a285315ce9ce42b7692cc5a13a9ffc95a4c0a8ed
Demeyer10 <04_zoster_borscht@icloud.com>
2024-04-20 20:22:42 -0400

    refactoring from main to another file

--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,10 @@
-use std::io::{self, stdout, Read};
-use termion::raw::IntoRawMode;
+#![warn(clippy::all, clippy::pedantic)]
+mod editor;
 
-fn to_ctrl_byte(c: char) -> u8 {
+use editor::Editor;
 
-    let byte = c as u8;
-    byte & 0b0001_1111
-        
-}
-fn die(e: std::io::Error) {
-    panic!("{}", e);
-}
 fn main() {
 
-    let _stdout = stdout().into_raw_mode().unwrap();
-
-
-    for b in io::stdin().bytes() {
-        match b {
-            Ok(b) => {
-                let c = b as char;
-                // if the character is a control character only print byte value otherwise print the byte
-                // and char value
-                if c.is_control() {
-                    println!("{:?} \r", b);
-                }else{
-                    println!("{:?} ({})\r", b, c);
-
-                }
-                    if b == to_ctrl_byte('q'){
-                        break;
-                    }
-            }
-        Err(err) => die(err),
-        }
-    }
+    let editor = Editor::defualt();
+    editor.run();
 }
