b1ae8a6938545c60bfb2111234daa10fd2c5a41e
Demeyer10 <04_zoster_borscht@icloud.com>
2024-04-23 16:27:38 -0400

    Open a file and viewing it contents on screen

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,14 @@
 #![warn(clippy::all, clippy::pedantic)]
 mod editor;
 mod terminal;
+mod document;
+mod row;
 
 
+pub use document::Document;
 use editor::Editor;
 pub use terminal::Terminal;
+pub use row::Row;
 pub use editor::Position;
 
 fn main() {
